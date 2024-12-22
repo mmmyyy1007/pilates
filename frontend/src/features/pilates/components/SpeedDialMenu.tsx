957b73033830b6d461398b1c922e2b9cc37c65f8
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2024-12-22 23:34:25 +0900

    メニュー表示変更

--- frontend/src/features/pilates/components/SpeedDialMenu.tsx
+++ frontend/src/features/pilates/components/SpeedDialMenu.tsx
@@ -2,25 +2,41 @@ import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import PersonIcon from "@mui/icons-material/Person";
 import PlaceIcon from "@mui/icons-material/Place";
 import { Box } from "@mui/material";
+import Backdrop from "@mui/material/Backdrop";
 import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import { useState } from "react";
 
 export const SpeedDialMenu = () => {
     const actions = [
-        { icon: <CalendarTodayIcon />, name: "Calendar" },
-        { icon: <PlaceIcon />, name: "Place" },
-        { icon: <PersonIcon />, name: "Person" },
+        { icon: <CalendarTodayIcon />, name: "レッスン一覧" },
+        { icon: <PlaceIcon />, name: "店舗一覧" },
+        { icon: <PersonIcon />, name: "アカウント管理" },
     ];
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <Box>
+            <Backdrop open={open} />
             <SpeedDial
                 ariaLabel="SpeedDial basic example"
                 sx={{ position: "absolute", bottom: 16, right: 16 }}
                 icon={<SpeedDialIcon />}
+                onClose={handleClose}
+                onOpen={handleOpen}
+                open={open}
             >
                 {actions.map((action) => (
-                    <SpeedDialAction key={action.name} icon={action.icon} tooltipTitle={action.name} />
+                    <SpeedDialAction
+                        key={action.name}
+                        icon={action.icon}
+                        tooltipTitle={action.name}
+                        tooltipOpen
+                        onClick={handleClose}
+                    />
                 ))}
             </SpeedDial>
         </Box>
