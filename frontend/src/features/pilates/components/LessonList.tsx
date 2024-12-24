5111d4b807720350ce06706f3f983a6e6c8fe36a
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2024-12-25 07:07:48 +0900

    レッスン一覧に日時フォーム追加

--- frontend/src/features/pilates/components/LessonList.tsx
+++ frontend/src/features/pilates/components/LessonList.tsx
@@ -1,10 +1,19 @@
 import { Typography } from "@/components/Typography";
 import { Box } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
 export const LessonList = () => {
     return (
         <Box sx={{ mt: 3 }}>
             <Typography variant="h5">レッスン一覧</Typography>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={["DateTimePicker"]}>
+                    <DateTimePicker label="Basic date time picker" />
+                </DemoContainer>
+            </LocalizationProvider>
         </Box>
     );
 };
