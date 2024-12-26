c01e2bd19ab8178cbbf9c4a1ad85b6a0c0fbed5f
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2024-12-26 16:56:49 +0900

    レッスン一覧フォーム表示

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
 import { theme } from "./configs/theme";
 import { AppRouter } from "./routes/AppRouter";
+
+dayjs.locale("ja");
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <AppRouter />
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ja">
+                <CssBaseline />
+                <AppRouter />
+            </LocalizationProvider>
         </ThemeProvider>
     );
 }
--- frontend/src/features/pilates/components/LessonList.tsx
+++ frontend/src/features/pilates/components/LessonList.tsx
@@ -1,19 +1,24 @@
+import { Button } from "@/components/Button";
+import { TextField } from "@/components/TextFiled";
 import { Typography } from "@/components/Typography";
 import { Box } from "@mui/material";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import Autocomplete from "@mui/material/Autocomplete";
+import { DateTimePicker } from "@mui/x-date-pickers";
 
 export const LessonList = () => {
+    const lessonPlace = ["STUDIO IVY 奥沢店"];
     return (
         <Box sx={{ mt: 3 }}>
             <Typography variant="h5">レッスン一覧</Typography>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DemoContainer components={["DateTimePicker"]}>
-                    <DateTimePicker label="Basic date time picker" />
-                </DemoContainer>
-            </LocalizationProvider>
+            <DateTimePicker label="開始日時" slotProps={{ textField: { size: "small", fullWidth: true } }} />
+            <DateTimePicker label="終了日時" slotProps={{ textField: { size: "small", fullWidth: true } }} />
+            <Autocomplete
+                disablePortal
+                options={lessonPlace}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="レッスン場所" />}
+            />
+            <Button variant="outlined">登録</Button>
         </Box>
     );
 };
