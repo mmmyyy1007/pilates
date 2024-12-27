740e38477eeab5507708e961493f344db6df483f
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2024-12-28 02:44:08 +0900

    レッスン一覧にカレンダー表示

--- frontend/src/features/pilates/components/LessonList.tsx
+++ frontend/src/features/pilates/components/LessonList.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/Button";
 import { TextField } from "@/components/TextFiled";
 import { Typography } from "@/components/Typography";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import FullCalendar from "@fullcalendar/react";
 import { Box } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
 import { DateTimePicker } from "@mui/x-date-pickers";
@@ -10,6 +12,14 @@ export const LessonList = () => {
     return (
         <Box sx={{ mt: 3 }}>
             <Typography variant="h5">レッスン一覧</Typography>
+            <FullCalendar
+                plugins={[dayGridPlugin]} // pluginsにdayGridPluginを設定する
+                // headerToolbar={{
+                //     right: "dayGridMonth,dayGridWeek",
+                // }}
+                initialView="dayGridMonth" // 初期表示のモードを設定する
+                events={"https://fullcalendar.io/api/demo-feeds/events.json"}
+            />
             <DateTimePicker label="開始日時" slotProps={{ textField: { size: "small", fullWidth: true } }} />
             <DateTimePicker label="終了日時" slotProps={{ textField: { size: "small", fullWidth: true } }} />
             <Autocomplete
