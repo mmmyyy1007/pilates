4194449160ecf5e88f68a2dd7ea49d6905352a03
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2024-12-29 11:44:03 +0900

    データ取得に並び順追加

--- backend/app/Place/Repositories/PlaceRepository.php
+++ backend/app/Place/Repositories/PlaceRepository.php
@@ -9,19 +9,27 @@ class PlaceRepository implements PlaceRepositoryInterface
 {
     /**
      * @param int $userId
-     * @return Collection
+     * @return Collection $records
      */
     public function getPlaceById(int $userId): Collection
     {
-        return Place::select('id', 'name', 'display_flag as displayFlag', 'order_no as orderNo')->where('user_id', $userId)->get();
+        $sql = Place::select('id', 'name', 'display_flag as displayFlag', 'order_no as orderNo')
+            ->where('user_id', $userId)
+            ->orderBy('order_no');
+
+        $records = $sql->get();
+
+        return $records;
     }
 
     /**
      * @param array $placeData
-     * @return bool
+     * @return bool $status
      */
     public function registerPlace(array $placeData): bool
     {
-        return Place::upsert($placeData, ['id', 'user_id'], ['name', 'display_flag', 'order_no']);
+        $status = Place::upsert($placeData, ['id', 'user_id'], ['name', 'display_flag', 'order_no']);
+
+        return $status;
     }
 }
