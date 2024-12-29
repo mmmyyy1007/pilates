76c734e80f7e3a238dd0831722b1cdbe40f21b28
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2024-12-29 11:23:29 +0900

    API用ルーティング変更

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PlaceController;
+use App\Place\Http\Controllers\PlaceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,11 +8,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< Updated upstream
-Route::post('/place/register', [PlaceController::class, 'register'])->middleware('auth');
-=======
 Route::middleware(['auth'])->prefix('place')->name('place.')->controller(PlaceController::class)->group(function () {
     Route::middleware(['convert.snake'])->post('register', 'register')->name('register');
     Route::get('show', 'show')->name('show');
 });
->>>>>>> Stashed changes
