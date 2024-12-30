343020bee9502438c8d4ab106614dd168c28f7ab
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2024-12-30 23:04:52 +0900

    レッスンとレッスン場所を分割して不要なため削除

--- frontend/src/features/pilates/api/showLessonPlace.ts
+++
@@ -1,7 +0,0 @@
-import { LessonPlaceApiResponse, LessonPlaceData } from "@/features/pilates/types/lessonTypes";
-import { apiClient } from "@/lib/apiClient";
-
-export const showLessonPlace = async (): Promise<LessonPlaceData[]> => {
-    const response = await apiClient.get<LessonPlaceApiResponse>("lesson/show");
-    return response.data.list.place;
-};
