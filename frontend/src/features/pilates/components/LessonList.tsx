2b3cefeeb4345187ec1c07876fbda8927d411e43
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2024-12-31 00:42:57 +0900

    開始日時と終了日時のデータ反映

--- frontend/src/features/pilates/components/LessonList.tsx
+++ frontend/src/features/pilates/components/LessonList.tsx
@@ -5,44 +5,60 @@ import { useLesson } from "@/features/pilates/hooks/useLesson";
 import { usePlace } from "@/features/pilates/hooks/usePlace";
 import { LessonData } from "@/features/pilates/types/lessonTypes";
 import { ActivePlaceData } from "@/features/pilates/types/placeTypes";
-// import allLocales from "@fullcalendar/core/locales-all";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
 import FullCalendar from "@fullcalendar/react";
 import { Box } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
 import { DateTimePicker } from "@mui/x-date-pickers";
-import { useEffect, useState } from "react";
+import dayjs, { Dayjs } from "dayjs";
+import { useCallback, useEffect, useState } from "react";
 
 export const LessonList = () => {
     const [ActivePlaceData, setActivePlaceData] = useState<ActivePlaceData[]>([]);
     const [LessonData, setLessonData] = useState<LessonData[]>([]);
     const { handleActiveShowPlace } = usePlace();
     const { handleShowLesson } = useLesson();
+    const [startDate, setStartDate] = useState<Dayjs | null>(dayjs(Date.now()));
+    const [endDate, setEndDate] = useState<Dayjs | null>(dayjs().add(1, "hour"));
     useEffect(() => {
         const fetchLessonData = async () => {
             const [lessonResponse, placeResponse] = await Promise.all([handleShowLesson(), handleActiveShowPlace()]);
-            console.log(lessonResponse);
+
             setActivePlaceData(placeResponse);
             setLessonData(lessonResponse);
         };
         fetchLessonData();
     }, []);
+    const handleDateClick = useCallback((arg: DateClickArg) => {
+        alert(arg.dateStr);
+    }, []);
     return (
         <Box sx={{ mt: 3 }}>
             <Typography variant="h5">レッスン一覧</Typography>
             <FullCalendar
-                plugins={[dayGridPlugin]} // pluginsにdayGridPluginを設定する
+                plugins={[dayGridPlugin, interactionPlugin]}
                 // locales={allLocales}
                 locale="ja"
                 // headerToolbar={{
                 //     right: "dayGridMonth,dayGridWeek",
                 // }}
                 initialView="dayGridMonth" // 初期表示のモードを設定する
-                // events={[{ title: "event", start: "2024-12-30T12:00:00", end: "2024-12-30T12:50:00" }]}
                 events={LessonData}
+                dateClick={handleDateClick}
             />
-            <DateTimePicker label="開始日時" slotProps={{ textField: { size: "small", fullWidth: true } }} />
-            <DateTimePicker label="終了日時" slotProps={{ textField: { size: "small", fullWidth: true } }} />
+            <DateTimePicker
+                label="開始日時"
+                slotProps={{ textField: { size: "small", fullWidth: true } }}
+                value={startDate}
+                onChange={(newStartDate) => setStartDate(newStartDate)}
+            />
+            <DateTimePicker
+                label="終了日時"
+                slotProps={{ textField: { size: "small", fullWidth: true } }}
+                value={endDate}
+                onChange={(newEndDate) => setEndDate(newEndDate)}
+            />
             <Autocomplete
                 disablePortal
                 noOptionsText=""
