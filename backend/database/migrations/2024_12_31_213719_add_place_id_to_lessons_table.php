765c9ad984a19537f344643dee994434bbd856cc
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2024-12-31 21:47:44 +0900

    lessonsテーブルにplace_idカラム追加

---
+++ backend/database/migrations/2024_12_31_213719_add_place_id_to_lessons_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->uuid('place_id')->nullable(false)->after('end_datetime'); // カラムを追加
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->dropColumn('place_id'); // カラムを削除
+        });
+    }
+};
