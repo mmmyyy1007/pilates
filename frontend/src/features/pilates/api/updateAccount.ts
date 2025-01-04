d6db04a5dbc3f305a5dbbbd6423b3e52df858fc0
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2025-01-05 00:11:55 +0900

    ファイル名変更のため削除

--- frontend/src/features/pilates/api/updateAccount.ts
+++
@@ -1,6 +0,0 @@
-import { UpdatedAccountData } from "@/features/pilates/types/accountTypes";
-import { apiClient } from "@/lib/apiClient";
-
-export const updateUser = async (data: UpdatedAccountData): Promise<void> => {
-    await apiClient.post<void>("account/update/user", data);
-};
