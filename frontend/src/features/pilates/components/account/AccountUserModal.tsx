9abf7afdc49925db83e693ce5131ceeabc7d97d6
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2025-01-16 00:11:04 +0900

    名前とメールアドレス変更モーダルに閉じるアイコンを表示

--- frontend/src/features/pilates/components/account/AccountUserModal.tsx
+++ frontend/src/features/pilates/components/account/AccountUserModal.tsx
@@ -3,6 +3,8 @@ import { Modal } from "@/components/Modal";
 import { TextField } from "@/components/TextFiled";
 import { Typography } from "@/components/Typography";
 import { AccountFormData } from "@/features/pilates/types/accountTypes";
+import CloseIcon from "@mui/icons-material/Close";
+import { Box, IconButton } from "@mui/material";
 
 interface AccountUserModalProps {
     openUser: boolean;
@@ -22,23 +24,34 @@ export const AccountUserModal = ({
     const { key, name, value } = updateFormName;
     return (
         <Modal open={openUser} onClose={() => setOpenUser(false)}>
-            <Typography>変更</Typography>
-            <TextField
-                type="text"
-                label={name}
-                value={value}
-                required
-                onChange={(e) =>
-                    setupdateFormName({
-                        key: key,
-                        name: name,
-                        value: e.target.value,
-                    })
-                }
-            ></TextField>
-            <Button variant="outlined" onClick={handleUpdate}>
-                更新
-            </Button>
+            <Typography>
+                {name}を入力してください。
+                <IconButton
+                    aria-label="close"
+                    onClick={() => setOpenUser(false)}
+                    style={{ position: "absolute", right: 4, top: 4 }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            </Typography>
+            <Box sx={{ mt: 3 }}>
+                <TextField
+                    type="text"
+                    label={name}
+                    value={value}
+                    required
+                    onChange={(e) =>
+                        setupdateFormName({
+                            key: key,
+                            name: name,
+                            value: e.target.value,
+                        })
+                    }
+                ></TextField>
+                <Button variant="outlined" onClick={handleUpdate}>
+                    更新
+                </Button>
+            </Box>
         </Modal>
     );
 };
