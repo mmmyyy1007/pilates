83dff8b728d6ab1ca2c6a13ad828dc0cd3ba34b1
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2025-01-16 00:03:10 +0900

    パスワード変更モーダルをStackでのレイアウト変更

--- frontend/src/features/pilates/components/account/AccountPasswordModal.tsx
+++ frontend/src/features/pilates/components/account/AccountPasswordModal.tsx
@@ -4,7 +4,7 @@ import { PasswordTextField } from "@/components/PasswordTextField";
 import { Typography } from "@/components/Typography";
 import { UpdatedPasswordData } from "@/features/pilates/types/accountTypes";
 import CloseIcon from "@mui/icons-material/Close";
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, Stack } from "@mui/material";
 import { ChangeEvent } from "react";
 
 interface AccountPasswordModalProps {
@@ -26,45 +26,44 @@ export const AccountPasswordModal = ({
     return (
         <>
             <Modal open={openPassword} onClose={() => setOpenPassword(false)}>
-                <Typography>
-                    現在のパスワードと新しいパスワードを入力してください。
-                    <IconButton
-                        aria-label="close"
-                        onClick={() => setOpenPassword(false)}
-                        style={{ position: "absolute", right: 4, top: 4 }}
-                    >
-                        <CloseIcon />
-                    </IconButton>
-                </Typography>
-                <PasswordTextField
-                    required
-                    label="現在のパスワード"
-                    value={password}
-                    name="password"
-                    sx={{ mt: 3 }}
-                    onChange={handleChangePasswordForm}
-                />
-                <PasswordTextField
-                    required
-                    label="新しいパスワード"
-                    value={newPassword}
-                    name="newPassword"
-                    sx={{ mt: 3 }}
-                    onChange={handleChangePasswordForm}
-                />
-                <PasswordTextField
-                    required
-                    label="新しいパスワード(確認用)"
-                    value={ConfirmNewPassword}
-                    name="ConfirmNewPassword"
-                    sx={{ mt: 3 }}
-                    onChange={handleChangePasswordForm}
-                />
-                <Box sx={{ mt: 3 }}>
-                    <Button variant="outlined" onClick={handleUpdatePass}>
-                        更新
-                    </Button>
-                </Box>
+                <Stack spacing={2}>
+                    <Typography>
+                        現在のパスワードと新しいパスワードを入力してください。
+                        <IconButton
+                            aria-label="close"
+                            onClick={() => setOpenPassword(false)}
+                            style={{ position: "absolute", right: 4, top: 4 }}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    </Typography>
+                    <PasswordTextField
+                        required
+                        label="現在のパスワード"
+                        value={password}
+                        name="password"
+                        onChange={handleChangePasswordForm}
+                    />
+                    <PasswordTextField
+                        required
+                        label="新しいパスワード"
+                        value={newPassword}
+                        name="newPassword"
+                        onChange={handleChangePasswordForm}
+                    />
+                    <PasswordTextField
+                        required
+                        label="新しいパスワード(確認用)"
+                        value={ConfirmNewPassword}
+                        name="ConfirmNewPassword"
+                        onChange={handleChangePasswordForm}
+                    />
+                    <Box>
+                        <Button variant="outlined" onClick={handleUpdatePass}>
+                            更新
+                        </Button>
+                    </Box>
+                </Stack>
             </Modal>
         </>
     );
