506343459eb0275721a68113aa38295d16cea047
mmmyyy1007 <179496343+mmmyyy1007@users.noreply.github.com>
2025-01-29 00:01:13 +0900

    スナックバーに閉じるアイコンの追加

--- frontend/src/components/Snackbar.tsx
+++ frontend/src/components/Snackbar.tsx
@@ -1,4 +1,5 @@
-import { Alert, Snackbar as MUISnackbar } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { Alert, IconButton, Snackbar as MUISnackbar } from "@mui/material";
 
 /**
  * スナックバーの型
@@ -12,7 +13,16 @@ interface SnackbarProps {
 export const Snackbar = ({ message, severity, onClose }: SnackbarProps) => {
     return (
         <MUISnackbar open={message !== null} onClose={onClose} autoHideDuration={5000}>
-            <Alert severity={severity} variant="filled" sx={{ width: "100%" }}>
+            <Alert
+                severity={severity}
+                variant="filled"
+                sx={{ width: "100%", display: "flex", alignItems: "center" }}
+                action={
+                    <IconButton size="small" aria-label="close" color="inherit" onClick={onClose}>
+                        <CloseIcon />
+                    </IconButton>
+                }
+            >
                 {message}
             </Alert>
         </MUISnackbar>
